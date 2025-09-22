f16aa2b5f15d0049ade1156ebf376099143558ea
Chukwuemeka-James <official.chukwuemekajames@gmail.com>
2025-09-22 18:23:08 +0100

    recipe dir: Fixed linting error

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -135,36 +135,7 @@ class TagViewSet(BaseRecipeAttrViewSet):
     queryset = Tag.objects.all()
 
 
-# class TagViewSet(mixins.DestroyModelMixin,
-#                 mixins.UpdateModelMixin,
-#                 mixins.ListModelMixin,
-#                 viewsets.GenericViewSet):
-#     """Manage tags in the database."""
-#     serializer_class = serializers.TagSerializer
-#     queryset = Tag.objects.all()
-#     authentication_classes = [TokenAuthentication]
-#     permission_classes = [IsAuthenticated]
-
-#     def get_queryset(self):
-#         """Filter queryset to authenticated users."""
-#         return self.queryset.filter(user=self.request.user).order_by('-name')
-
 class IngredientViewSet(BaseRecipeAttrViewSet):
       """Manage ingredients in the database."""
       serializer_class = serializers.IngredientSerializer
       queryset = Ingredient.objects.all()
-
-
-# class IngredientViewSet(mixins.DestroyModelMixin,
-#                         mixins.UpdateModelMixin,
-#                         mixins.ListModelMixin,
-#                         viewsets.GenericViewSet):
-#      """Manage ingredients in the database."""
-#      serializer_class = serializers.IngredientSerializer
-#      queryset = Ingredient.objects.all()
-#      authentication_classes = [TokenAuthentication]
-#      permission_classes = [IsAuthenticated]
-
-#      def get_queryset(self):
-#         """Filter queryset to authenticated users."""
-#         return self.queryset.filter(user=self.request.user).order_by('-name')
